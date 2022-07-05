49d18c278c95562ad16b9e78d21cee4a99643576
unknown <bernardsamson2003@yahoo.com>
2022-07-05 22:05:56 +0100

    First Push for the Simple JavaScript

---
+++ main.js
@@ -0,0 +1,17 @@
+//alert("connected");
+//console.log("Hello World");
+let fullName = "Akpoji Bernard";
+
+console.log(fullName);
+
+let height = "1.75m";
+
+console.log(height);
+
+let country = "Nigeria"
+
+console.log(country);
+
+let myinformation = "My name is " + fullName + ", I am " + height + " tall, I am from " + country + ".";
+
+console.log(myinformation);
